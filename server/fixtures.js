397779d87a956d94f296c2fedfa008400cb38931
v.fedoseev <vl.fedoseev@gmail.com>
2015-08-24 22:32:53 +0300

    Wire collection into 'postsList' template

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,20 +1,17 @@
-var postsData = [
-	{
+if(Posts.find().count() === 0) {
+	Posts.insert({
 		title: 'Introducing Telscope',
 		url: 'http://sachagreif.com/introducing-telescope/'
-	},
-	{
+	});
+
+	Posts.insert({
 		title: 'Meteor',
 		url: 'http://meteor.com'
-	},
-	{
+	});
+
+	Posts.insert({
 		title: 'The Meteor Book',
 		url: 'http://themeteorbook.com'
-	}
-];
+	});
 
-Template.postsList.helpers({
-	posts: function() {
-		return Posts.find();
-	}
-});
+}
