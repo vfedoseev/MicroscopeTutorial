0f642cc2e10fe4fc89bef1254664a7756f20ffde
v.fedoseev <vl.fedoseev@gmail.com>
2015-08-25 22:34:51 +0300

    Add basic routing

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,11 @@
-Meteor.publish('posts', function(){
+//publish uses _publishCursor() when returning a cursor
+Meteor.publish('posts', function(author){
 	return Posts.find();
+	return Posts.find({flagged: false, author: author});
+});
+
+Meteor.publish('allPosts', function(author){
+	return Posts.find({author: author}, {fields: {
+		date: false
+	}});
 });
