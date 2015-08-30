db0ead3f91f53aeab8cdb6d9e6a82d02a15b02d4
v.fedoseev <vl.fedoseev@gmail.com>
2015-08-30 22:21:06 +0300

    Use method for create new post

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -6,8 +6,12 @@ Template.postSubmit.events({
 			url: $(e.target).find('[name=url]').val(),
 			title: $(e.target).find('[name=title]').val()
 		};
-
-		post._id = Posts.insert(post);
-		Router.go('postPage', post);
+		Meteor.call('postInsert', post, function(error, result) {
+			if(error) {
+				return alert(error.reason);
+			}
+			Router.go('postPage', {_id: result._id});
+		});
+		
 	}
 });
