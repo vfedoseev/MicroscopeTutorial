a8a7bd41b5bdca58fc088572b9e31fc85801d69f
v.fedoseev <vl.fedoseev@gmail.com>
2015-09-05 23:33:17 +0300

    Add comments: collection, pub and sub

--- server/publications.js
+++ server/publications.js
@@ -4,6 +4,9 @@ Meteor.publish('posts', function(author){
 	return Posts.find({flagged: false, author: author});
 });
 
+Meteor.publish('comments', function(){
+	return Comments.find();
+});
 Meteor.publish('allPosts', function(author){
 	return Posts.find({author: author}, {fields: {
 		date: false
