eae40bf8f76d4867b4273cc83ba9925f93cd710e
v.fedoseev <vl.fedoseev@gmail.com>
2015-09-06 11:12:17 +0300

    Add form for comments

---
+++ client/views/comments/comment_submit.js
@@ -0,0 +1,20 @@
+Template.commentSubmit.events({
+	'submit form': function(e,template) {
+		e.preventDefault();
+
+		var $body = $(e.target).find('[name=body]');
+		var comment = {
+			body: $body.val(),
+			postId: template.data._id
+		};
+
+		Meteor.call('comment', comment, function(error, commentId) {
+			if(error) {
+				Errors.throw(error.reason);
+			}
+			else {
+				$body.val('');
+			}
+		})
+	}
+});
--- packages/errors/errors.js
+++ packages/errors/errors.js
@@ -2,7 +2,6 @@ Errors = {
 	collection: new Meteor.Collection(null), 
 
 	throw: function(message) {
-		console.log('message');
 		Errors.collection.insert({message: message, seen: false});
 	},
 	clearSeen: function() {
