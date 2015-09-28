5cdc3a51194e3c25017a3bb922300181c56f2f65
v.fedoseev <vl.fedoseev@gmail.com>
2015-09-29 00:58:26 +0300

    Add animation to posts rearrange

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,3 +1,6 @@
+var POST_HEIGHT = 80;
+var Positions = new Meteor.Collection(null);
+
 Template.postItem.helpers({
 	ownPost: function() {
 		return this.userId == Meteor.userId();
@@ -15,6 +18,24 @@ Template.postItem.helpers({
 		else {
 			return 'btn-primary disabled';
 		}
+	},
+	attributes: function() {
+		var post = _.extend({}, Positions.findOne({postId: this._id}), this);
+		var newPosition = post._rank * POST_HEIGHT;
+		var attributes = {};
+
+		if(! _.isUndefined(post.position)) {
+			var offset = post.position - newPosition;
+			attributes.style = "top:" + offset + "px";
+			if(offset === 0 ) {
+				attributes.class = "post animate";
+			}
+		}
+		Meteor.setTimeout(function() {
+			Positions.upsert({postId: post._id}, {$set: {position: newPosition}});
+		});
+
+		return attributes;
 	}
 });
 
--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -17,4 +17,11 @@ Template.postsList.helpers({
 	/*posts: function() {
 		return Posts.find({},{sort: {submitted: -1}});
 	}*/
+	postsWithRank: function() {
+		this.posts.rewind();
+		return this.posts.map(function(post,index, cursor) {
+			post._rank = index;
+			return post;
+		});
+	}
 });
